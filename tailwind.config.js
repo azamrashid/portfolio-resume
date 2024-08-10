b5bea80e9ca312e0957e7f41837a082969ee1a62
Azam Rashid <azamrashid@gmail.com>
2024-08-11 00:24:30 +0500

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily:{
